0b686b2dff2e2c305881cf0dc6eeec72d11a7141
sar0j <xyzpro2018@gmail.com>
2026-03-30 20:02:45 +1100

    Update app to v1.3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app =  Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps World 🚀 v1.2"
+    return "Hello DevOps World 🚀 v1.3"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
